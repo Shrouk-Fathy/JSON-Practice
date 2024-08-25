edbb214ccee91ab5f900371a3fd862b9096f2afb
shrouk <shrmhfathy.99@gmail.com>
2024-08-25 20:28:51 +0300

    upload

---
+++ Fetch data from json file using JQeury/main.js
@@ -0,0 +1,42 @@
+
+// ----------------- 1 -Fetch Data From Json File Using jQuery --------------------
+
+$(document).ready(function () {
+  $.getJSON("products.json", function (data) {
+    $.each(data, function (index, product) {
+      const productContainer = document.createElement("div");
+      const productImg = document.createElement("img");
+      const productTitle = document.createElement("h2");
+      const productDescription = document.createElement("p");
+      const productPrice = document.createElement("span");
+      const addToCart = document.createElement("span");
+
+      productContainer.className = "product";
+      productImg.className = ".productImg";
+      productTitle.className = "title";
+      productDescription.className = "description";
+      productPrice.className = "price";
+      addToCart.className = "addToChart";
+
+      productImg.src = product.image;
+      productTitle.innerText = product.title;
+      productDescription.innerText = product.description;
+      productPrice.innerText = product.price + "$";
+      addToCart.innerHTML = `Add To Cart 
+        <lord-icon
+            src="https://cdn.lordicon.com/guothkao.json"
+            trigger="loop-on-hover"
+            stroke="bold"
+            style="width:30px;height:30px;margin-left:5px">
+       </lord-icon>`;
+
+      productContainer.append(productImg);
+      productContainer.append(productTitle);
+      productContainer.append(productDescription);
+      productContainer.append(productPrice);
+      productContainer.append(addToCart);
+
+      $("#product-list").append(productContainer);
+    });
+  });
+});
